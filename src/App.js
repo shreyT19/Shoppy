11342f7900d1c3e341d84d1d7ea355928cf27225
shreyT19 <shreyanshtripathy49@gmail.com>
2023-06-23 01:04:27 +0530

    added initial files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import { useEffect } from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {FiSettings} from 'react-icons/fi'
+
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+     
+     <BrowserRouter>
+      <div className='flex relative dark:bg-main-dark-bg'>
+        <div className='fixed right-4 bottom-4' style={{zIndex:'1000'}}>
+          <TooltipComponent content="Settings" position='top'>
+            <button type='button' className='text-3xl p-3 hover:drop-shadow-xl'>
+              <FiSettings/>
+            </button>
+          </TooltipComponent>
+        </div>
+      </div>
+     </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
