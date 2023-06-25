03588a380c9c708b757c318dbf2b48ecf6b108df
shreyT19 <shreyanshtripathy49@gmail.com>
2023-06-25 14:02:46 +0530

    added navbar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,7 +8,7 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { links } from "../data/dummy";
 import { useStateContext } from "../context/ContextProvider";
 const SideBar = () => {
-  const {activeMenu,setActiveMenu} = useStateContext();
+  const {activeMenu,setActiveMenu,screenSize} = useStateContext();
 
   
   // Active Link CSS
@@ -19,6 +19,15 @@ const SideBar = () => {
   const normalLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
+  //handle closing the side bar whenever you click on any of the content
+  // for mobile devices only
+
+  const handleCloseSideBar = ()=>{
+    if(screenSize<=900 && activeMenu){
+      setActiveMenu(false)
+    }
+  }
+
   return (
     <div className="ml-3 h-screen  md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -27,7 +36,7 @@ const SideBar = () => {
             <Link
               to="/"
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSideBar}
             >
               <SiShopware />
               <span>Shoppy</span>
@@ -56,7 +65,7 @@ const SideBar = () => {
                       <NavLink
                         key={link.name}
                         to={`/${link.name}`}
-                        onClick={() => {}}
+                        onClick={handleCloseSideBar}
                         className={({ isActive }) => (isActive ? activeLink : normalLink)}
                       >
                         {link.icon}
--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState,useEffect } from "react";
 
 export const StateContext = createContext();
 
@@ -11,15 +11,40 @@ const initialState = {
 };
 
 export const ContextProvider = ({ children }) => {
+  //for menu
   const [activeMenu, setActiveMenu] = useState(true);
+
+  //for button click actions
+  const [isClicked, setIsClicked] = useState(initialState);
+
+  //screenSize state
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  //handling button click actions
+  const handleClick = (clickedElement) => {
+    setIsClicked({ ...initialState, [clickedElement]: true });
+  };
+
+  
+
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
 };
 
 //custom hook to call the useContext hook
-// instead of importing "useContext" and passing "StateContext" we have initialized a custom hook to speed up the process and reduce unnecessary imports  
+// instead of importing "useContext" and passing "StateContext" we have initialized a custom hook to speed up the process and reduce unnecessary imports
 
 export const useStateContext = () => useContext(StateContext);
