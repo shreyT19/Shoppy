b0c09f3834b6f3b911889fe59b262f976baaaf9b
shreyT19 <shreyanshtripathy49@gmail.com>
2023-07-01 21:06:29 +0530

    fixed some issues

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -18,7 +18,7 @@ const Ecommerce = () => {
   return (
     <div className="mt-12">
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
-        <div
+        {/* <div
           className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center"
         >
           <div className="flex justify-between items-center">
@@ -36,7 +36,7 @@ const Ecommerce = () => {
               borderRadius="10px"
             />
           </div>
-        </div>
+        </div> */}
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
           {earningData.map((item) => {
             return (
